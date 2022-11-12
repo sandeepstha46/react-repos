960b191464cb3948c85ddb9598c4196d664b0f5b
sandeepstha46 <sandeepstha64@gmail.com>
2022-11-12 12:10:23 +0545

    testimonials section completed

--- Ecommerce Website/src/components/Testimonials/Testimonials.jsx
+++ Ecommerce Website/src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import css from './Testimonials.module.css'
 import Hero from '../../assets/testimonialHero.png'
+import { TestimonialsData } from '../../data/testimonials'
+import { Swiper, SwiperSlide } from "swiper/react"
 
 const Testimonials = () => {
     return (
@@ -16,6 +18,26 @@ const Testimonials = () => {
                     <span>100k</span>
                     <span>Happy Customers with us</span>
                 </div>
+            </div>
+            <div className={css.reviews}>
+                Reviews
+            </div>
+
+            <div className={css.carousel}>
+                <Swiper slidesPerView={3} slidesPerGroup={1} spaceBetween={20} className={css.tCarousel} loop={true}>
+                    {
+                        TestimonialsData.map((testimonial, i) => (
+                            <SwiperSlide>
+                                <div className={css.testimonial}>
+                                    <img src={testimonial.image} alt="" />
+                                    <span>{testimonial.comment}</span>
+                                    <hr />
+                                    <span>{testimonial.name}</span>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
             </div>
         </div>
     )
